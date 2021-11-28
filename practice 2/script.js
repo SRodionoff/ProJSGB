aa5713b0fb19bfab1ae67260a718ceb5b40d2a52
SRodionoff <87777879+SRodionoff@users.noreply.github.com>
2021-11-28 04:47:48 +0300

    Add files via upload

---
+++ practice 2/script.js
@@ -0,0 +1,85 @@
+// const goods = [
+//     { title: 'Shirt', price: 150 },
+//     { title: 'Socks', price: 50 },
+//     { title: 'Jacket', price: 350 },
+//     { title: 'Shoes', price: 250 },
+// ];
+
+// const $goodsList = document.querySelector('.goods-list');
+
+// const renderGoodsItem = ({ title, price }) => {
+//     return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+// };
+
+// const renderGoodsList = (list = goods) => {
+//     let goodsList = list.map(
+//             (item) =>  {
+//                 return renderGoodsItem(item)
+//             }
+//         ).join('');
+
+//     $goodsList.insertAdjacentHTML('beforeend', goodsList);
+// }
+
+// renderGoodsList();
+
+class GoodsItem {
+  constructor(title, price) {
+    this.title = title;
+    this.price = price;
+  }
+
+  render() {
+    return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+  }
+}
+
+
+
+class GoodsList {
+  constructor() {
+    this.goods = [];
+  }
+
+  fetchGoods() {
+    this.goods = [
+      { title: 'Shirt', price: 150 },
+      { title: 'Socks', price: 50 },
+      { title: 'Jacket', price: 350 },
+      { title: 'Shoes', price: 250 },
+    ];
+    //вычисление суммы товаров
+    let sum = 0;
+    for (let total of this.goods) {
+      sum += total.price;
+    };
+    alert(sum);
+  };
+
+  render() {
+    let listHtml = '';
+    this.goods.forEach(good => {
+      const goodItem = new GoodsItem(good.title, good.price);
+      listHtml += goodItem.render();
+    });
+    document.querySelector('.goods-list').innerHTML = listHtml;
+  };
+}
+//добавление пустых классов
+
+class promocode {
+
+};
+class discount {
+
+};
+class personalBonus {
+
+};
+
+
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
+
+
